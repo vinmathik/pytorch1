9e16d1793e9a4ec41e5df3e650b09b64eea3789d
vinmathik <113250460+vinmathik@users.noreply.github.com>
2023-07-19 17:03:47 +0530

    Update FullyConvolutionalResnet18.py

--- image classification/FullyConvolutionalResnet18.py
+++ image classification/FullyConvolutionalResnet18.py
@@ -48,47 +48,24 @@ with torch.no_grad():
         preds = model(image)
         preds = torch.softmax(preds, dim=1)
         print('Response map shape : ', preds.shape)
-
-        # Find the class with the maximum score in the n x m output map
         pred, class_idx = torch.max(preds, dim=1)
         print(class_idx)
-
-
         row_max, row_idx = torch.max(pred, dim=1)
         col_max, col_idx = torch.max(row_max, dim=1)
         predicted_class = class_idx[0, row_idx[0, col_idx], col_idx]
-        
-        # Print top predicted class
         print('Predicted Class : ', image[predicted_class], predicted_class)
-
-        # Find the n x m score map for the predicted class
-        score_map = preds[0, predicted_class, :, :].cpu().numpy()
+         score_map = preds[0, predicted_class, :, :].cpu().numpy()
         score_map = score_map[0]
-        
-        # Resize score map to the original image size
         score_map = cv2.resize(score_map, (original_image.shape[1], original_image.shape[0]))
-
-        # Binarize score map
         _, score_map_for_contours = cv2.threshold(score_map, 0.25, 1, type=cv2.THRESH_BINARY)
         score_map_for_contours = score_map_for_contours.astype(np.uint8).copy()
-
-        # Find the countour of the binary blob
         contours, _ = cv2.findContours(score_map_for_contours, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
-        
-        # Find bounding box around the object. 
         rect = cv2.boundingRect(contours[0])
-
-        # Apply score map as a mask to original image
         score_map = score_map - np.min(score_map[:])
         score_map = score_map / np.max(score_map[:])
-
         score_map = cv2.cvtColor(score_map, cv2.COLOR_GRAY2BGR)
         masked_image = (original_image * score_map).astype(np.uint8)
-
-        # Display bounding box
         cv2.rectangle(masked_image, rect[:2], (rect[0] + rect[2], rect[1] + rect[3]), (0, 0, 255), 2)
-        
-        # Display images
         cv2.imshow("Original Image", original_image)
         cv2.imshow("scaled_score_map", score_map)
         cv2.imshow("activations_and_bbox", masked_image)
